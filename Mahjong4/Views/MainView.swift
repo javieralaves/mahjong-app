f73e342f1da28d994a366e9318e436b319e1b012
Javier Alaves <jayalaves@gmail.com>
2025-07-08 16:00:37 +0800

    PH1-UI: render player hand

--- Mahjong4/Views/MainView.swift
+++ Mahjong4/Views/MainView.swift
@@ -4,13 +4,20 @@ struct MainView: View {
     @StateObject private var gameState = GameState()
 
     var body: some View {
-        Text("Mahjong4")
-            .font(.largeTitle)
-            .padding()
-            .onAppear {
-                gameState.shuffleWall()
-                gameState.dealInitialHands()
-            }
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Mahjong4")
+                .font(.largeTitle)
+
+            Text("Player 1 Hand")
+                .font(.headline)
+
+            TileRowView(tiles: gameState.players.first?.hand ?? [])
+        }
+        .padding()
+        .onAppear {
+            gameState.shuffleWall()
+            gameState.dealInitialHands()
+        }
     }
 }
 
---
+++ Mahjong4/Views/TileView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+/// Displays a single Mahjong tile using placeholder text.
+struct TileView: View {
+    let tile: Tile
+    var body: some View {
+        Text(tile.displayString)
+            .font(.caption)
+            .frame(width: 40, height: 60)
+            .background(Color.white)
+            .cornerRadius(4)
+            .shadow(radius: 1)
+    }
+}
+
+struct TileView_Previews: PreviewProvider {
+    static var previews: some View {
+        TileView(tile: .bamboo(1))
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray.opacity(0.2))
+    }
+}
